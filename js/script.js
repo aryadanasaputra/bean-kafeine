159f5cc22540147c7f09f412e2ecd31e03729fd3
aryadanasaputra <arya.dana.saputra01@gmail.com>
2024-06-08 11:28:18 +0700

    UPDATE

--- js/script.js
+++ js/script.js
@@ -4,10 +4,23 @@ document.querySelector('#hamburger-menu').onclick = () => {
     navbarNav.classList.toggle('active');
 };
 
+const searchForm = document.querySelector('.search-form')
+const searchBox = document.querySelector('#search-box')
+const searchbtn = document.querySelector('#search-btn')
+document.querySelector('#search-btn').onclick = (e) => {
+    searchForm.classList.toggle('active');
+    searchBox.focus();
+    // e.preventDefault();
+};
+
+
 const hamburger = document.querySelector('#hamburger-menu');
 document.addEventListener('click', function (e) {
     if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
         navbarNav.classList.remove('active');
+    }
+    if (!searchbtn.contains(e.target) && !searchForm.contains(e.target)) {
+        searchForm.classList.remove('active');
     }
 });
 
