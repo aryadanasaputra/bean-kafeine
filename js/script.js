c8f1c4c8c3dab6c6c7e9026fe2c719982583b3b9
aryadanasaputra <arya.dana.saputra01@gmail.com>
2024-06-09 20:27:19 +0700

    Update1

--- js/script.js
+++ js/script.js
@@ -1,20 +1,53 @@
-// Toggle class active
+// Toggle class active navbar Humburger Menu
+const hamburger = document.querySelector('#hamburger-menu');
 const navbarNav = document.querySelector('.navbar-nav');
-document.querySelector('#hamburger-menu').onclick = () => {
+document.querySelector('#hamburger-menu').onclick = (e) => {
     navbarNav.classList.toggle('active');
+    e.preventDefault();
 };
 
+// Search
 const searchForm = document.querySelector('.search-form')
 const searchBox = document.querySelector('#search-box')
 const searchbtn = document.querySelector('#search-btn')
 document.querySelector('#search-btn').onclick = (e) => {
     searchForm.classList.toggle('active');
     searchBox.focus();
-    // e.preventDefault();
+    e.preventDefault();
 };
 
+// Shopping cart
+const shoppingBtn = document.querySelector('#shopping-cart-btn')
+const shoppingCart = document.querySelector('.shopping-cart')
+document.querySelector('#shopping-cart-btn').onclick = (e) => {
+    shoppingCart.classList.toggle('active');
+    e.preventDefault();
+}
 
-const hamburger = document.querySelector('#hamburger-menu');
+// Modal Box
+const itemDetailModal = document.querySelector('#item-detail-modal');
+const itemDetailBtns = document.querySelectorAll('.shopping-icon');
+
+itemDetailBtns.forEach((btn) => {
+    btn.onclick = (e) => {
+        itemDetailModal.style.display = 'flex';
+        e.preventDefault();
+    };
+});
+
+const modalCloseIcon = document.querySelector('.modal .close-icon');
+document.querySelector('.modal .close-icon').onclick = (e) => {
+    itemDetailModal.style.display = 'none';
+    e.preventDefault();
+}
+
+window.onclick = (e) => {
+    if (e.target === itemDetailModal) {
+        itemDetailModal.style.display = 'none';
+    }
+}
+
+// Remove active class
 document.addEventListener('click', function (e) {
     if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
         navbarNav.classList.remove('active');
@@ -22,13 +55,17 @@ document.addEventListener('click', function (e) {
     if (!searchbtn.contains(e.target) && !searchForm.contains(e.target)) {
         searchForm.classList.remove('active');
     }
+    if (!shoppingBtn.contains(e.target) && !shoppingCart.contains(e.target)) {
+        shoppingCart.classList.remove('active');
+    }
 });
 
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
+// Smooth sroll
+// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//     anchor.addEventListener('click', function (e) {
+//         e.preventDefault();
+//         document.querySelector(this.getAttribute('href')).scrollIntoView({
+//             behavior: 'smooth'
+//         });
+//     });
+// });
